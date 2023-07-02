d5a096fce696337b76f311d32fcf6f24ac0ea4df
Masudd_11 <69025871+masudd11@users.noreply.github.com>
2023-07-02 12:12:12 +0530

    fourth commit form first branch (#3)

--- auto.py
+++ auto.py
@@ -1,5 +1,5 @@
 # first commit
-# third commit form secod branch
+#thise commit form first branch
 import pyautogui as pag
 pag.doubleClick(150, 150)
 pag.write('This Desktop is Automated!', interval=0.25)
